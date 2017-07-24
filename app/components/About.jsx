5e628937bb908d2316ce003516151a117352d955
Thanawat Hirunyanitiwattana <oat_war_time@hotmail.com>
2017-07-24 14:44:49 +0700

    Styling About page

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -12,7 +12,20 @@ let About = (props) => {
   return (
     <div>
       <h3 className="text-center">About</h3>
-      <p>Welcome to About page</p>
+      <p>
+        This is a weather application build on React. I have build this for The Complete React Web App Developer Course.
+      </p>
+      <p>
+        Here are some of the tools I used.
+      </p>
+      <ul>
+        <li>
+          <a href="https://facebook.github.io/react">React</a> - This was the JavaScript framework used.
+        </li>
+        <li>
+          <a href="https://openweathermap.org">Open Weather Map</a> - I used Open Weather Map to search for weather data by city name.
+        </li>
+      </ul>
     </div>
   );
 };
